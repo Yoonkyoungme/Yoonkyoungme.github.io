65bc8179ea9a3b841c265d922a9e33e3abb80fb8
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-09-22 01:15:28 +0900

    feat: description 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -14,11 +14,7 @@ module.exports = {
     name: `김윤경`,
     bio: {
       role: `개발자`,
-      description: [
-        '성장과 경험을 추구하는',
-        '세상에 가치를 더하는',
-        '사용자 중심의 개발을 추구하는',
-      ],
+      description: ['성장과 경험을 추구하는', '세상에 가치를 더하는', '사용자 중심의'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
